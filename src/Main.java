4bb8a7b914bce20873a0ee1d9bef6806a1df6a4d
Dmitriy_Korchagin <793070407232@ya.ru>
2023-02-02 00:32:01 +0300

    homework complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = new Calculator();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        /*
+        int на 0 делить нельзя, чтобы избежать ArithmeticException
+        тип данных должен быть с плавающей точкой.
+        */
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
     }
 }
