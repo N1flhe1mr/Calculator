5d42a313c2a08799f9525308d10de35e286ede1a
Dmitriy_Korchagin <793070407232@ya.ru>
2023-02-02 18:56:50 +0300

    gitignore fix

---
+++ src/DevideByZeroException.java
@@ -0,0 +1,5 @@
+public class DevideByZeroException extends Exception {
+    public DevideByZeroException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DevideByZeroException {
         Calculator calc = new Calculator();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
@@ -7,7 +7,11 @@ public class Main {
         int на 0 делить нельзя, чтобы избежать ArithmeticException
         тип данных должен быть с плавающей точкой.
         */
-        int c = calc.devide.apply(a, b);
-        calc.println.accept(c);
+        if (b != 0) {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } else {
+            throw new DevideByZeroException("Can't divide by zero!!!");
+        }
     }
 }
